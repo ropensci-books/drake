625187e133e8d34019b27b818a490e22f8c1fb67
wlandau <will.landau@gmail.com>
2018-06-10 19:17:14 -0400

    Start putting all the figures inline

--- build.R
+++ build.R
@@ -1,3 +1,10 @@
+library(fs)
+if (file.exists("docs")){
+  dir_delete("docs")
+}
+dir_create("docs")
+dir_copy("images", "docs")
+
 bookdown::render_book(
   input = "index.Rmd", 
   output_format = "bookdown::gitbook",
