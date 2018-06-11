abdebfbf78d55df67d64cce36707cac3911fbca5
wlandau <will.landau@gmail.com>
2018-06-10 20:13:19 -0400

    Add images and rebuild
    Also remove refs to old vignettes

--- build.R
+++ build.R
@@ -5,6 +5,44 @@ if (file.exists("docs")){
 dir_create("docs")
 dir_copy("images", "docs")
 
+faq <- function(){
+  library(tidyverse)
+  library(gh)
+  
+  is_faq <- function(label){
+    identical(label$name, "frequently asked question")
+  }
+  
+  any_faq_label <- function(issue){
+    any(vapply(issue$labels, is_faq, logical(1)))
+  }
+  
+  faq <- gh(
+    "GET /repos/ropensci/drake/issues?state=all",
+    .limit = Inf
+  ) %>%
+    Filter(f = any_faq_label)
+  
+  combine_fields <- function(lst, field){
+    map_chr(lst, function(x){
+      x[[field]]
+    })
+  }
+  
+  titles <- combine_fields(faq, "title")
+  urls <- combine_fields(faq, "html_url")
+  links <- paste0("- [", titles, "](", urls, ")")
+
+  dest <- "13-faq.Rmd"
+  tmp <- file.copy(from = "faq-stub.md", to = dest, overwrite = TRUE)
+  
+  con <- file(dest, "a")
+  writeLines(c("", links), con)
+  close(con)
+}
+
+faq()
+
 bookdown::render_book(
   input = "index.Rmd", 
   output_format = "bookdown::gitbook",
---
+++ faq-stub.md
@@ -0,0 +1,3 @@
+# Frequently-asked questions {#faq}
+
+This FAQ is a compendium of [pedagogically useful issues tagged on GitHub](https://github.com/ropensci/drake/issues?q=is%3Aissue+is%3Aopen+label%3A%22frequently+asked+question%22). To contribute, please [submit a new issue](https://github.com/ropensci/drake/issues/new) and ask that it be labeled a frequently asked question.
