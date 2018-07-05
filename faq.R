6ccc0785fa7ea7c03ced140877160e9b72a933dd
wlandau-lilly <will.landau@lilly.com>
2018-07-05 16:06:43 -0400

    Add faq.R

---
+++ faq.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(gh)
+
+is_faq <- function(label){
+  identical(label$name, "frequently asked question")
+}
+
+any_faq_label <- function(issue){
+  any(vapply(issue$labels, is_faq, logical(1)))
+}
+
+faq <- gh(
+  "GET /repos/ropensci/drake/issues?state=all",
+  .limit = Inf
+) %>%
+  Filter(f = any_faq_label)
+
+combine_fields <- function(lst, field){
+  map_chr(lst, function(x){
+    x[[field]]
+  })
+}
+
+titles <- combine_fields(faq, "title")
+urls <- combine_fields(faq, "html_url")
+links <- paste0("- [", titles, "](", urls, ")")
+
+dest <- "14-faq.Rmd"
+file.copy(from = "faq-stub.md", to = dest, overwrite = TRUE)
+
+con <- file(dest, "a")
+writeLines(c("", links), con)
+close(con)
