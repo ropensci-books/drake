63dda980f1879a99bfb42fb51c5bd0b379741ed1
wlandau-lilly <will.landau@lilly.com>
2018-07-05 16:03:21 -0400

    Separate the FAQ

--- build.R
+++ build.R
@@ -1,46 +1,9 @@
 library(fs)
-if (file.exists("docs")){
-  dir_delete("docs")
+dir <- "_book"
+if (file.exists(dir)){
+  dir_delete(dir)
 }
-dir_create("docs")
-dir_copy("images", "docs")
-
-faq <- function(){
-  library(tidyverse)
-  library(gh)
-
-  is_faq <- function(label){
-    identical(label$name, "frequently asked question")
-  }
-
-  any_faq_label <- function(issue){
-    any(vapply(issue$labels, is_faq, logical(1)))
-  }
-
-  faq <- gh(
-    "GET /repos/ropensci/drake/issues?state=all",
-    .limit = Inf
-  ) %>%
-    Filter(f = any_faq_label)
-
-  combine_fields <- function(lst, field){
-    map_chr(lst, function(x){
-      x[[field]]
-    })
-  }
-
-  titles <- combine_fields(faq, "title")
-  urls <- combine_fields(faq, "html_url")
-  links <- paste0("- [", titles, "](", urls, ")")
-
-  dest <- "14-faq.Rmd"
-  file.copy(from = "faq-stub.md", to = dest, overwrite = TRUE)
-
-  con <- file(dest, "a")
-  writeLines(c("", links), con)
-  close(con)
-}
-
-faq()
+dir_create(dir)
+dir_copy("images", dir)
 drake::clean(destroy = TRUE)
 bookdown::render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
