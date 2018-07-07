1e6c5be4a923e72f901264a663faab374638c7bb
wlandau <will.landau@gmail.com>
2018-07-07 10:37:28 -0400

    Try with correct ref to gh api token

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-echo "begin deploying"
+echo "Begin deploying."
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
+[ -z "${GITHUB_TOKEN}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+echo "Found token and on master branch."
 
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git clone -b gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 cd gh-pages
 cp -r ../_book/* ./
 git add --all *
 git commit -m "Update the manual" || true
 git push -q origin gh-pages
-echo "done deploying"
+echo "Done deploying."
