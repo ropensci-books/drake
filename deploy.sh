21db5d088f502351d62036eef9d513948db163e7
wlandau <will.landau@gmail.com>
2018-07-07 10:57:42 -0400

    Rename token

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 echo "Begin deploying."
 set -e
 
-[ -z "${GITHUB_TOKEN}" ] && exit 0
+[ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-echo "Found token and on master branch."
+echo "Found gh token and on master branch."
 
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
 
-git clone -b gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 cd gh-pages
 cp -r ../_book/* ./
 git add --all *
