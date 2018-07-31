20d7bbaeae12e89cd83f743bc441a3d5a858a012
wlandau-lilly <will.landau@lilly.com>
2018-07-31 13:41:24 -0400

    Need to check GITHUB_PAT first
    Otherwise, we can't access the environment variables
    we need to run deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ -z "${GITHUB_PAT}" ] && exit 0
 echo "Environment variables:"
 echo "  PATH: ${PATH}"
 echo "  TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
@@ -6,7 +7,6 @@ echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
-  [ -z "${GITHUB_PAT}" ] && exit 0
   echo "Deploying to production."
   git config --global user.email "will.landau@gmail.com"
   git config --global user.name "wlandau"
