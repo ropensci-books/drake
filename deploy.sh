7e3d0659f54f8706de1de438fd74a6a6fae3d0ae
wlandau-lilly <will.landau@lilly.com>
2018-07-31 08:51:50 -0400

    Try changing deploy script
    Working up to Netlify previews

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
-echo "Begin deploying."
-set -e
-
+#!/bin/bash
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-echo "Found gh token and on master branch."
-
-git config --global user.email "will.landau@gmail.com"
-git config --global user.name "wlandau"
-
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
-cd gh-pages
-cp -r ../_book/* ./
-git add --all *
-git commit -m "Update the manual" || true
-git push -q origin gh-pages
-echo "Done deploying."
+if [[ "${TRAVIS_BRANCH}" -eq "master" ]]
+then
+  echo "Deploying to production."
+  git config --global user.email "will.landau@gmail.com"
+  git config --global user.name "wlandau"
+  git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+  cd gh-pages
+  cp -r ../_book/* ./
+  git add --all *
+  git commit -m "Update the manual" || true
+  git push -q origin gh-pages
+elif [[ "${TRAVIS_PULL_REQUEST_BRANCH}" -neq "" ]]
+then
+  echo "Deploying Netlify preview."
+  cp -r ../_book/* ./
+  netlify deploy -t ${NETLIFYKEY} --draft
+fi
