ef9afc961bbcd250ed7ecae02061cb7a46bdd65b
wlandau-lilly <will.landau@lilly.com>
2018-07-31 11:48:04 -0400

    Move checking of GITHUB_PAT

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-[ -z "${GITHUB_PAT}" ] && exit 0
 echo "Environment variables:"
 echo "  PATH: ${PATH}"
 echo "  TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
@@ -7,6 +6,7 @@ echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
+  [ -z "${GITHUB_PAT}" ] && exit 0
   echo "Deploying to production."
   git config --global user.email "will.landau@gmail.com"
   git config --global user.name "wlandau"
