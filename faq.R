8ac9a449cbb68e246f0d0a8f5e54fbe9027638bf
wlandau-lilly <will.landau@lilly.com>
2018-10-17 15:25:47 -0400

    Fix #45

--- faq.R
+++ faq.R
@@ -9,22 +9,26 @@ any_faq_label <- function(issue){
   any(vapply(issue$labels, is_faq, logical(1)))
 }
 
-faq <- gh(
-  "GET /repos/ropensci/drake/issues?state=all",
-  .limit = Inf
-) %>%
-  Filter(f = any_faq_label)
-
 combine_fields <- function(lst, field){
   map_chr(lst, function(x){
     x[[field]]
   })
 }
 
-titles <- combine_fields(faq, "title")
-urls <- combine_fields(faq, "html_url")
-links <- paste0("- [", titles, "](", urls, ")")
+build_faq <- function(){
+  faq <- gh(
+    "GET /repos/ropensci/drake/issues?state=all",
+    .limit = Inf
+  ) %>%
+    Filter(f = any_faq_label)
+  titles <- combine_fields(faq, "title")
+  urls <- combine_fields(faq, "html_url")
+  links <- paste0("- [", titles, "](", urls, ")")
+  con <- file("faq.Rmd", "a")
+  writeLines(c("", links), con)
+  close(con)
+}
 
-con <- file("faq.Rmd", "a")
-writeLines(c("", links), con)
-close(con)
+if (!identical(Sys.getenv("TRAVIS_PULL_REQUEST"), "true")){
+  build_faq()
+}
