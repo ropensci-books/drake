57c2ed2ac06c2cdcd8980c2328e56944a102ef3a
wlandau <will.landau@gmail.com>
2019-02-23 19:04:04 -0500

    Add icons properly

--- build.R
+++ build.R
@@ -4,8 +4,6 @@ if (file.exists(dir)){
   dir_delete(dir)
 }
 dir_create(dir)
-for (file in list.files("favicon", full.names = TRUE)) {
-  file.copy(file, dir)
-}
+dir_copy("images", dir)
 drake::clean(destroy = TRUE)
 bookdown::render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
