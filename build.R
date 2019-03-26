a3520bd03d139a7e1a281f99ea55584156a3706a
wlandau-lilly <will.landau@lilly.com>
2019-03-26 09:26:05 -0400

    Try to build pdf version

--- build.R
+++ build.R
@@ -5,5 +5,21 @@ if (file.exists(dir)){
 }
 dir_create(dir)
 dir_copy("images", dir)
+options(drake_make_menu = FALSE)
 drake::clean(destroy = TRUE)
-bookdown::render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
+bookdown::render_book(
+  input = "index.Rmd",
+  output_format = "bookdown::gitbook"
+)
+try({
+  drake::clean(destroy = TRUE)
+  bookdown::render_book(
+    input = "index.Rmd",
+    output_format = "bookdown::pdf_book",
+    output_dir = "_pdfbook"
+  )
+  file.copy(
+    from = "_pdfbook/ropensci-dev-guide.pdf",
+    to = "_book/ropensci-dev-guide.pdf"
+  )
+})
